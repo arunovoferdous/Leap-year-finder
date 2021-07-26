0bf7f7da3b6c862aad5bc25785032c7e4b0a3cc3
arunovoferdous <87965345+arunovoferdous@users.noreply.github.com>
2021-07-26 12:02:50 +0545

    Add files via upload

---
+++ LeapYearFinder.py
@@ -0,0 +1,4 @@
+print("If you want to know that which year is a leap year and which one is not, then Adro is here to help you" )
+year = int(input("Type the year : "))
+print( year%4==0)
+print("Thank you sir, have a nice day")
